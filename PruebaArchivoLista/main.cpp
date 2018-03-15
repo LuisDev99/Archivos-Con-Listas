43a51141eef507534d033fceb264e89cc3806005
LuisDev99 <luisgpr99@gmail.com>
2018-03-15 14:34:10 -0600

    Add project files.

---
+++ PruebaArchivoLista/main.cpp
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include "UtilidadesListaEnlazada.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+
+	nodo head = 0;
+
+
+	//Numeros se ordenar de menor a mayor!
+
+	UtilidadesListaEnlazada::insertarFinal(head, 2);
+	UtilidadesListaEnlazada::insertarFinal(head, 7);
+	UtilidadesListaEnlazada::insertarFinal(head, 3);
+
+	cout << "\n === Lista desordenada === "<<endl;
+
+	UtilidadesListaEnlazada::printList(head);
+	UtilidadesListaEnlazada::ordenarLista(head);
+
+	cout << "\n === Lista Ordenada === " << endl;
+	UtilidadesListaEnlazada::printList(head);
+
+	nodo nodeAE = UtilidadesListaEnlazada::crearLista(8);
+
+	cout << "\n === Lista desordenada creada === " << endl;
+	UtilidadesListaEnlazada::printList(nodeAE);
+
+	UtilidadesListaEnlazada::ordenarLista(nodeAE);
+
+	cout << "\n === Lista ordenada === " << endl;
+
+	UtilidadesListaEnlazada::printList(nodeAE);
+
+
+
+	char * nombre = new char;
+	cout << "Nombre con la extension! : ";
+	cin >> nombre;
+
+	UtilidadesListaEnlazada::guardarLista(nodeAE, nombre);
+
+	cout << "Presione cualquier tecla para continuar";
+	_getch();
+
+}
